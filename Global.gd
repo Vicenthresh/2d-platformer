ec12dc295a0c1068d18d9785d0b182ce8cafa5f0
Vicente Solis <vicente.solis@usach.cl>
2023-06-22 00:47:01 -0400

    Window size fix

--- Global.gd
+++ Global.gd
@@ -1,10 +1,10 @@
 extends Node
 
-var gui = get_node("res://HUD")
+#var gui = get_node("res://HUD")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	gui.scale = Vector2(4, 4)
+#	gui.scale = Vector2(4, 4)
 	pass # Replace with function body.
 
 
--- levels/level.gd
+++ levels/level.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
-#@onready var gui = $CanvasLayer
+@onready var gui = $CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-#	gui.scale = Vector2(4, 4)
+	gui.scale = Vector2(4, 4)
 	pass # Replace with function body.
 
 
