35d51405fd7b5c0f075b4d2f3233fc8340fcea67
Vicenthresh <vicente.solis@usach.cl>
2024-05-26 01:18:40 -0400

    coyote_jump, dash added

--- Player/player.gd
+++ Player/player.gd
@@ -1,20 +1,26 @@
 extends CharacterBody2D
 
 
-var SPEED = 25.0
-var STOPPING_SPEED = 15.0
-var JUMP_VELOCITY = -300.0
-var max_running_speed: = 180
-var max_air_speed = max_running_speed * 0.9
+@export var SPEED: float = 25.0
+@export var STOPPING_SPEED: float = 15.0
+@export var JUMP_VELOCITY: float = -300.0
+@export var max_running_speed: float = 180
+@export var max_air_speed: float = max_running_speed * 0.9
+@export var coyote_time: float = 0.1
+
 var direction
 var knockback: = Vector2.ZERO
 var knockbackTween
+var can_jump: bool = true
 
 var state: = 'normal'
 var max_hp: = 3
 var current_hp = max_hp
 var max_jumps = 1
 var jumps_left = max_jumps
+var can_dash: bool = false
+var dashing: bool = false
+var dash_direction = Vector2(1,0)
 
 signal hp_changed(current_hp)
 
@@ -22,7 +28,7 @@ signal hp_changed(current_hp)
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animSprite = $AnimatedSprite2D
-@onready var label = $Label
+#@onready var label = $Label
 @onready var collision = $CollisionShape2D
 # Sounds
 @onready var jump_sound = $JumpSound
@@ -40,25 +46,44 @@ func _physics_process(delta):
 	var max_speed
 	# Add the gravity
 	if not is_on_floor():
+		if can_jump:
+			get_tree().create_timer(coyote_time).timeout.connect(coyoteTimeout)
 		if velocity.y > 0:
 			velocity.y += gravity*1.5 * delta
-		else:
+		elif !dashing:
 			velocity.y += gravity * delta
 		SPEED = 30.0
 		max_speed = max_air_speed
 	else:
+		can_jump = true
+		can_dash = true
 		jumps_left = max_jumps
 		max_speed = max_running_speed
 
 	# Handle Jump.
-	if (Input.is_action_just_pressed("jump")) and jumps_left > 0:
+	if (Input.is_action_just_pressed("jump")) and jumps_left > 0 and can_jump:
 #		if abs(velocity.x) == max_running_speed:
 #			velocity.y = JUMP_VELOCITY - (abs(velocity.x) * 0.2)
 #			max_air_speed = 200
 #		else:
 		jump_sound.play()
 		velocity.y = JUMP_VELOCITY
+		#velocity.y = JUMP_VELOCITY - (abs(velocity.x) * 0.2)		
 		jumps_left -= 1
+		if(jumps_left == 0):
+			can_jump = false
+	
+	if Input.is_action_just_pressed("dash") and can_dash:
+		if (animSprite.flip_h):
+			dash_direction = Vector2(-1,0)
+		else:
+			dash_direction = Vector2(1,0)
+			
+		velocity = dash_direction.normalized() * 500
+		can_dash = false
+		dashing = true
+		await get_tree().create_timer(0.3).timeout
+		dashing = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -76,7 +101,7 @@ func _physics_process(delta):
 	
 	if state == 'normal':
 		get_animation(velocity)
-	label.text = str(velocity)
+	#label.text = str(velocity)
 	
 	for idx in range(get_slide_collision_count()):
 		var collision = get_slide_collision(idx)
@@ -86,26 +111,10 @@ func _physics_process(delta):
 func get_animation(velocity):
 	if velocity.y > 0:
 		animSprite.play('fall')
-	if velocity.x > 0:
-#		collision.position = Vector2(-0.5, -0.25)
-		if velocity.y < 0:
-			animSprite.play("jump")
-		elif velocity.y > 0:
-			animSprite.play('fall')
-		else:
-			animSprite.play('run')
-	elif velocity.x < 0:
-#		collision.position = Vector2(0.5, -0.25)
-		if velocity.y < 0:
-			animSprite.play("jump")
-		elif velocity.y > 0:
-			animSprite.play('fall')
-		else:
-			animSprite.play('run')
-	elif velocity.y < 0:
-		animSprite.play("jump")
-	elif velocity.y > 0:
-		animSprite.play('fall')
+	elif velocity.y < 0 or not is_on_floor():
+		animSprite.play('jump')
+	elif velocity.x != 0:
+		animSprite.play('run')
 	else:
 		animSprite.play('idle')
 		
@@ -135,3 +144,6 @@ func _damage_finished():
 	if current_hp == 0:
 		die()
 	state = 'normal'
+
+func coyoteTimeout():
+	can_jump = false
