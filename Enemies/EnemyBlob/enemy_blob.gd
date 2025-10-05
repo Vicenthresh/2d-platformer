f2249189998682310592d152f96921b54ac5ebdc
Vicente Solis <vicente.solis@usach.cl>
2025-10-05 18:56:35 -0300

    enemy behavior

--- Enemies/EnemyBlob/enemy_blob.gd
+++ Enemies/EnemyBlob/enemy_blob.gd
@@ -1,31 +1,50 @@
 extends CharacterBody2D
 
-const JUMP_SPEED = 300
-const GRAVITY = 600
+var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var JUMP_VELOCITY: float = -300.0
 
 @onready var anim_player = $AnimationPlayer
 @onready var animation_tree = $AnimationTree
+@onready var jump_timer = $JumpTimer
+
+var jump = false
 
 var is_jumping = false
 
 func _ready():
 	animation_tree.active = true
-	print('tesstestestestestset')
-	print(anim_player.animation_started)
-
-
-func _process(delta):
-	await get_tree().create_timer(0.6).timeout
-	is_jumping = !is_jumping
 	
+#func _process(delta: float) -> void:
+	#jump_behavior()
 
 func _physics_process(delta):
 	# Apply gravity
-
-	#if is_on_floor():
-		#velocity.y = -JUMP_SPEED
-		
-
 	move_and_slide()
-	animation_tree.set("parameters/conditions/is_jumping", is_jumping)
-	animation_tree.set("parameters/conditions/not_jumping", not is_jumping)
+	update_animation_parameters(velocity)
+	
+	#if jump:
+		#velocity.y = JUMP_VELOCITY
+	
+	if not is_on_floor():
+		_handle_air_physics(delta)
+		
+		
+#func jump_behavior():
+	#jump = true
+	#jump_timer.start()
+	
+func _handle_air_physics(delta: float):
+	if velocity.y > 0:
+		velocity.y += GRAVITY * 1.5 * delta
+	
+	
+func update_animation_parameters(vel: Vector2):
+	animation_tree.set("parameters/conditions/on_floor", is_on_floor())
+	animation_tree.set("parameters/conditions/is_idle", is_on_floor() and abs(vel.x) < 1)
+	animation_tree.set("parameters/conditions/is_jumping", vel.y < 0 and not is_on_floor())
+	animation_tree.set("parameters/conditions/is_falling", vel.y > 0.5 and not is_on_floor())
+	animation_tree.set("parameters/conditions/is_landing", is_on_floor() and vel.y >= 0)
+
+
+#func _on_jump_timer_timeout() -> void:
+	#jump = false
